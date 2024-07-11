6e631ab26d40085bc6f6b9626c96379965dbf653
fran gregori <frangregori61@gmail.com>
2024-07-12 00:51:31 +0200

    ADD - role, film models and migrations

---
+++ database/migrations/2024_07_11_221450_create_films_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title')->max(100);
+            $table->string('sumary')->max(255);
+            $table->smallInteger('year');
+            $table->string('director')->max(100);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films');
+    }
+};
