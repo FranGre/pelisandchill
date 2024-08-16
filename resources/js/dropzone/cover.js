e694d498be767667b6e22ae18661edf40747fe4f
fran gregori <frangregori61@gmail.com>
2024-08-16 21:26:38 +0200

    Send cover to back correctly :)

--- resources/js/dropzone/cover.js
+++ resources/js/dropzone/cover.js
@@ -4,5 +4,8 @@ import "dropzone/dist/dropzone.css"
 Dropzone.autoDiscover = false
 
 let dropzone = new Dropzone("#cover", {
-    url: "/upload/cover"
+    url: "/upload/cover",
+    headers: {
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
+    }
 })
