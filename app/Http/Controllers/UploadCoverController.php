48c356e2e708ef8c33e7b2174a5d294678261e40
fran gregori <frangregori61@gmail.com>
2024-08-19 20:37:28 +0200

    Upload and Save cover to local and Cover Table

--- app/Http/Controllers/UploadCoverController.php
+++ app/Http/Controllers/UploadCoverController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cover;
 use Illuminate\Http\Request;
+use Storage;
 
 class UploadCoverController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $cover = $request->file;
+
+        if (!Storage::directoryExists('covers')) {
+            Storage::makeDirectory('covers');
+        }
+
+        $folder = uniqid('cover-');
+        $path = 'covers/' . $folder;
+        $filename = $cover->getClientOriginalName();
+
+        Storage::putFileAs($path, $cover, $filename);
+
+        Cover::create([
+            'filename' => $filename,
+            'path' => $path,
+            'folder' => $folder
+        ]);
     }
 }
